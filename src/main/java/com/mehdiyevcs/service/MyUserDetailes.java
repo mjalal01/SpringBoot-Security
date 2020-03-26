da93ec1a0bbaee0cde9c8c57c631abdaf408f6f9
Jalal Mekhtiyev <djalal.mekhtiyev@gmail.com>
2020-03-26 23:46:44 +0400

    Remove Unused Imports

--- src/main/java/com/mehdiyevcs/service/MyUserDetailes.java
+++ src/main/java/com/mehdiyevcs/service/MyUserDetailes.java
@@ -2,14 +2,11 @@ package com.mehdiyevcs.service;
 
 import com.mehdiyevcs.model.User;
 import com.mehdiyevcs.repository.UserRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
